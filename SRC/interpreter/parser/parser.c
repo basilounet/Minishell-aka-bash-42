dd9984d8c1abd32ef4605a454f05c5296c31fcea
Gabriel Angouillant <gangouil@2C3.42angouleme.fr>
2024-03-22 18:02:01 +0100

    parser bonuses leak free

--- SRC/interpreter/parser/parser.c
+++ SRC/interpreter/parser/parser.c
@@ -70,15 +70,21 @@ static int	parenth_check(t_tokens *tokens)
 	while (tokens)
 	{
 		if (balance < 0)
-			return (0); // syntax error
+			break;
 		if (tokens->symbol == T_LPARENTH)
 			balance++;
 		if (tokens->symbol == T_RPARENTH)
 			balance--;
 		tokens = tokens->next;
 	}
-	if (balance != 0)
-		return (0); // syntax error
+	if (balance > 0 || balance < 0)
+	{
+		if (balance > 0)
+			printf("baseshell: syntax error near unexpected token `('\n");
+		if (balance < 0)
+			printf("baseshell: syntax error near unexpected token `)'\n");
+		return (0);
+	}
 	return (1);
 }
 
@@ -89,19 +95,40 @@ t_node	*parse_prompt(t_tokens **tokens)
 	if (!parenth_check(*tokens))
 		return (NULL);
 	node = parse_logex(tokens);
+	if (node && *tokens)
+	{
+		printf("baseshell: syntax error near unexpected token `%s'\n", symbol_to_char(*tokens));
+		free_node(node);
+		return (NULL);
+	}
 	return (node);
 }
 
 t_node	*parse_logex(t_tokens **tokens)
 {
     t_node		*node;
+	t_node		*right;
+	e_symbol	symbol;
 
 	node = parse_pipeline(tokens);
-	if (!node)
-		return (NULL);
-	while (*tokens && ((*tokens)->symbol == T_OR || (*tokens)->symbol == T_AND))
+	while (node && *tokens && ((*tokens)->symbol == T_OR
+		|| (*tokens)->symbol == T_AND))
 	{
-		//build a droite dans un tree
+		symbol = (*tokens)->symbol;
+		free(ft_tokpop(tokens));
+		if (!*tokens)
+		{
+			printf("baseshell: syntax error near unexpected token `newline'\n");
+			free_node(node);
+			return (NULL);
+		}
+		right = parse_pipeline(tokens);
+		if (!right)
+		{
+			free_node(node);
+			return (NULL);
+		}
+		node = ft_treenew(symbol, node, right, NULL);
 	}
 	return (node);
 }
@@ -111,17 +138,13 @@ t_node *parse_pipeline(t_tokens **tokens)
 	t_node	*node;
 	t_node	*right;
 
-	//check for brace group
-	//if one, node type = T_TREE
 	node = parse_command(tokens);
-	if (!node)
-		return (NULL);
-	if (*tokens && (*tokens)->symbol == T_PIPE)
+	if (node && *tokens && (*tokens)->symbol == T_PIPE)
 	{
 		free(ft_tokpop(tokens));
-		if (!(*tokens))
+		if (!*tokens)
 		{
-			printf("baseshell: unexpected token near |\n");
+			printf("baseshell: syntax error near unexpected token `newline'\n");
 			free_node(node);
 			return (NULL);
 		}
@@ -140,10 +163,23 @@ t_node		*parse_command(t_tokens **tokens)
 {
 	t_node		*node;
 
-	if (*tokens && (*tokens)->symbol == T_LPARENTH) //ne pas oublier de pop la parenthese
+	if (*tokens && is_symbol_set(3, 0, (*tokens)->symbol, T_PIPE, T_AND, T_OR))
+	{
+		printf("baseshell: syntax error near unexpected token `%s'\n", symbol_to_char(*tokens));
+		return (NULL);
+	}
+	if (*tokens && (*tokens)->symbol == T_LPARENTH)
 		node = parse_brace(tokens);
 	else
+	{
 		node = parse_simple_command(tokens);
+		if (node && *tokens && (*tokens)->symbol == T_LPARENTH)
+		{
+			printf("baseshell: syntax error near unexpected token `('\n");
+			free_node(node);
+			return (NULL);
+		}
+	}
 	return (node);
 }
 
@@ -177,9 +213,23 @@ t_node   *parse_simple_command(t_tokens **tokens)
 
 t_node *parse_brace(t_tokens **tokens)
 {
-	(void)tokens;
-    return (NULL); // call to parse_logex
-		// + bails mystiques tmtc
+	t_node	*node;
+
+	free(ft_tokpop(tokens));
+	if (*tokens && (*tokens)->symbol == T_RPARENTH)
+	{
+		printf("baseshell: syntax error near unexpected token `)'\n");
+		return (NULL);
+	}
+	node = parse_logex(tokens);
+	if (*tokens && (*tokens)->symbol == T_RPARENTH)
+		free(ft_tokpop(tokens));
+	if (*tokens && node && parse_redlist(node, tokens) == 0)
+	{
+		free_node(node);
+		return (NULL);
+	}
+    return (node);
 }
 
 int	parse_redlist(t_node *node, t_tokens **tokens)
